9ac325214789b29900374f063c85f97cb70dff09
wanghaibing <wanghaibing@kezaihui.com>
2019-02-27 17:56:42 +0800

    4.7.0.21-R

---
+++ mondrian/src/generated/java/mondrian/olap4j/MondrianOlap4jDriverVersion.java
@@ -0,0 +1,15 @@
+/*
+ * Project version information. Generated - do not modify.
+ */
+package mondrian.olap4j;
+/**
+ * Version information for the Mondrian olap4j driver. (Generated.)
+ */
+class MondrianOlap4jDriverVersion {
+    static final String NAME = "Mondrian olap4j driver";
+    static final String VERSION = "8.2.0.0-SNAPSHOT";
+    static final int MAJOR_VERSION = 8;
+    static final int MINOR_VERSION = 000;
+}
+
+// End MondrianOlap4jDriverVersion.java
---
+++ mondrian/src/generated/java/mondrian/resource/MondrianResource_de_DE.java
@@ -0,0 +1,24 @@
+// This class is generated. Do NOT modify it, or
+// add it to source control.
+
+package mondrian.resource;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import org.eigenbase.resgen.*;
+
+/**
+ * This class was generated
+ * by class org.eigenbase.resgen.ResourceGen
+ * from /Users/oukaihei/dev/workspace/zaihui/mondrian/mondrian/src/main/java/mondrian/resource/MondrianResource.xml
+ * on Sun Feb 24 16:57:24 CST 2019.
+ * It contains a list of messages, and methods to
+ * retrieve and format those messages.
+ */
+
+public class MondrianResource_de_DE extends MondrianResource {
+    public MondrianResource_de_DE() throws IOException {
+    }
+}
+
+// End MondrianResource_de_DE.java
